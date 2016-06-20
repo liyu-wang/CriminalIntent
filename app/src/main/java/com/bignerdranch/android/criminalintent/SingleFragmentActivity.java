ed1a40b99dde80e376197cd842185efcaf1cdad8
liyu-wang <liyu.wang@oasis.com>
2016-06-20 23:39:13 +1000

    added an abstract single fragment activity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by liyuw on 20/06/2016.
+ */
+abstract public class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
