909f8ddf39c8c36a0aef9d79ae7af7a1c260e451
liyu-wang <liyu.wang@oasis.com>
2016-07-03 21:39:02 +1000

    added the initial db for the crimes lab

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,9 @@ public class CrimeLab {
 
     private List<Crime> mCrimes;
 
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
@@ -22,6 +28,9 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+
         mCrimes = new ArrayList<>();
     }
 
